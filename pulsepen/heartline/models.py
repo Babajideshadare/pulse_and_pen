bd84c3870e61055138a2c9b0ddea107501abc7bc
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-24 12:01:46 +0100

    Add BPEntry model and apply migrations

--- pulsepen/heartline/models.py
+++ pulsepen/heartline/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class BPEntry(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    systolic = models.IntegerField()
+    diastolic = models.IntegerField()
+    pulse = models.IntegerField()
+    note = models.TextField(blank=True, null=True)
