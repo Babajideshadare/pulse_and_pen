d9f16adcdfa2d1d705346a6520605f398c7d09d9
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-25 12:42:30 +0100

    Add class-based CRUD views for BPEntry

--- pulsepen/heartline/views.py
+++ pulsepen/heartline/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+from .models import BPEntry
+
+class BPEntryListVeiw(ListView):
+    model = BPEntry
+    template_name = 'heartline/bpentries_list.html'
+    context_object_name = 'bpentries'
+    ordering = ('-date', '-time')
+
+class BPEntryDetailView(DetailView):
+    model = BPEntry
+    template_name = 'heartline/bpentries_detail.html'
+
+class BPEntryCreateView(CreateView):
+    model = BPEntry
+    fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
+    template_name = 'heartline/bpentries_create.html'
+    success_url = reverse_lazy(BPEntryListVeiw)
+
+class BPEntryUpdateView(UpdateView):
+    model = BPEntry
+    fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
+    template_name = 'heartline/bpentries_update.html'
+    success_url = reverse_lazy(BPEntryListVeiw)
+
+class BPEntryDeleteView(DeleteView):
+    model = BPEntry
+    template_name = 'heartline/bpentries_confirm_delete.html'
+    success_url = reverse_lazy(BPEntryListVeiw)
