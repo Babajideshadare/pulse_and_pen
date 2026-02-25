acae5bed963f4e695c84ba1aa3a6ee6688cfc2de
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-25 12:57:33 +0100

    Implement and refine BPEntry CRUD class-based views

--- pulsepen/heartline/views.py
+++ pulsepen/heartline/views.py
@@ -3,29 +3,29 @@ from django.urls import reverse_lazy
 
 from .models import BPEntry
 
-class BPEntryListVeiw(ListView):
+class BPEntryListView(ListView):
     model = BPEntry
-    template_name = 'heartline/bpentries_list.html'
-    context_object_name = 'bpentries'
+    template_name = 'heartline/bpentry_list.html'
+    context_object_name = 'bp_entries'
     ordering = ('-date', '-time')
 
 class BPEntryDetailView(DetailView):
     model = BPEntry
-    template_name = 'heartline/bpentries_detail.html'
+    template_name = 'heartline/bpentry_detail.html'
 
 class BPEntryCreateView(CreateView):
     model = BPEntry
     fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
-    template_name = 'heartline/bpentries_create.html'
-    success_url = reverse_lazy(BPEntryListVeiw)
+    template_name = 'heartline/bpentry_form.html'
+    success_url = reverse_lazy('bpentry-list')
 
 class BPEntryUpdateView(UpdateView):
     model = BPEntry
     fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
-    template_name = 'heartline/bpentries_update.html'
-    success_url = reverse_lazy(BPEntryListVeiw)
+    template_name = 'heartline/bpentry_form.html'
+    success_url = reverse_lazy('bpentry-list')
 
 class BPEntryDeleteView(DeleteView):
     model = BPEntry
-    template_name = 'heartline/bpentries_confirm_delete.html'
-    success_url = reverse_lazy(BPEntryListVeiw)
+    template_name = 'heartline/bpentry_confirm_delete.html'
+    success_url = reverse_lazy('bpentry-list')
