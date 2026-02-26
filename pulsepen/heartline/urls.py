a5274dd77fe18282c2a7e83a3af4ea34e2b6177e
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-26 15:40:08 +0100

    Add HTML auth: register, login, logout with navbar logout button

--- pulsepen/heartline/urls.py
+++ pulsepen/heartline/urls.py
@@ -13,6 +13,8 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='heartline/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='heartline/logout.html'), name='logout'),
     path('register/', register, name ='register'),
+
+    path('bp/', BPEntryListView.as_view(), name='bpentry-list'),
     path('bp/new/', BPEntryCreateView.as_view(), name='bpentry-create'),
     path('bp/<int:pk>/', BPEntryDetailView.as_view(), name='bpentry-detail'),
     path('bp/<int:pk>/update/', BPEntryUpdateView.as_view(), name='bpentry-update'),
