0f869404b5de7c12c7305f188ad073e1fe451e66
Babajide_shadare <shadarebabaj@gmail.com>
2026-02-27 15:12:33 +0100

    Restrict BP entries to logged-in users and link entries to owner

--- pulsepen/heartline/views.py
+++ pulsepen/heartline/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
 
@@ -23,24 +25,47 @@ class BPEntryListView(ListView):
     template_name = 'heartline/bpentry_list.html'
     context_object_name = 'bp_entries'
     ordering = ('-date', '-time')
+    login_url = 'login'
 
-class BPEntryDetailView(DetailView):
+    def get_queryset(self):
+        return BPEntry.objects.filter(user=self.request.user).order_by('-date', '-time')
+
+
+
+class BPEntryDetailView(LoginRequiredMixin, DetailView):
     model = BPEntry
     template_name = 'heartline/bpentry_detail.html'
+    login_url = 'login'
 
-class BPEntryCreateView(CreateView):
+    def get_queryset(self):
+        return BPEntry.objects.filter(user=self.request.user)
+
+class BPEntryCreateView(LoginRequiredMixin, CreateView):
     model = BPEntry
     fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
     template_name = 'heartline/bpentry_form.html'
     success_url = reverse_lazy('bpentry-list')
+    login_url = 'login'
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
 class BPEntryUpdateView(UpdateView):
     model = BPEntry
     fields = ('date', 'time', 'systolic', 'diastolic', 'pulse', 'note')
     template_name = 'heartline/bpentry_form.html'
     success_url = reverse_lazy('bpentry-list')
+    login_url = 'login'
 
-class BPEntryDeleteView(DeleteView):
+    def get_queryset(self):
+        return BPEntry.objects.filter(user=self.request.user)
+
+class BPEntryDeleteView(LoginRequiredMixin, DeleteView):
     model = BPEntry
     template_name = 'heartline/bpentry_confirm_delete.html'
     success_url = reverse_lazy('bpentry-list')
+    login_url = 'login'
+
+    def get_queryset(self):
+        return BPEntry.objects.filter(user=self.request.user)
