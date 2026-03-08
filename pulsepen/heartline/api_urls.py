cf1f00145d2f5c275ae8d1d86f4745a890a7fb4e
Babajide_shadare <shadarebabaj@gmail.com>
2026-03-08 16:38:02 +0100

    Add /api/token/ endpoint using DRF obtain_auth_token (Day 3 Checker 4)

--- pulsepen/heartline/api_urls.py
+++ pulsepen/heartline/api_urls.py
@@ -1,8 +1,12 @@
+from django.urls import path
 from rest_framework.routers import DefaultRouter
+from rest_framework.authtoken.views import obtain_auth_token
 from .api_views import BPEntryViewSet, JournalEntryViewSet
 
 router = DefaultRouter()
 router.register(r'bp-entries', BPEntryViewSet, basename='bp-entry')
 router.register(r'journal-entries', JournalEntryViewSet, basename='journal-entry')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('token/', obtain_auth_token, name='api-token'),
+] + router.urls
